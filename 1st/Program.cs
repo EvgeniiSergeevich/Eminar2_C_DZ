162770cb2e4e5756afaabc2f94a9cd85d5daf6ad
Evgeniy <zheinirk38@gmail.com>
2022-09-06 16:32:41 +0800

    Completed task 1

---
+++ 1st/Program.cs
@@ -0,0 +1,12 @@
+﻿try {
+    Console.WriteLine("Enter a three-digit number: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Digit(x));
+
+} catch {
+    Console.WriteLine("ENTER THE INTEGER NUMBER!!1");
+}
+int Digit(int x) {
+    int d = (x - x % 10 - (x / 100) * 100) / 10;
+    return d;
+}
