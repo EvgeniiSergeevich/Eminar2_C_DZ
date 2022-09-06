aab722c18cacd98fb2a9aeb17784972020722b7a
Evgeniy <zheinirk38@gmail.com>
2022-09-06 17:15:02 +0800

    Completed task 2

---
+++ 2nd/Program.cs
@@ -0,0 +1,16 @@
+﻿try {
+    Console.WriteLine("Enter the integer number: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    ThirdDigit(x);
+} catch {
+    Console.WriteLine("Try again!");
+}
+
+void ThirdDigit(int x) {
+    string number = x.ToString();
+    if(number.Length >= 3) {
+        Console.WriteLine(number[2]);
+    } else {
+        Console.WriteLine("No third digit!");
+    }    
+}
