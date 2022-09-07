e9cfc751c4700edae133dc1ee25ebf4c7821ab63
Evgeniy <zheinirk38@gmail.com>
2022-09-07 11:09:03 +0800

    added ThirdDigit method

--- 2nd/Program.cs
+++ 2nd/Program.cs
@@ -1,16 +1,28 @@
 ﻿try {
     Console.WriteLine("Enter the integer number: ");
     int x = Convert.ToInt32(Console.ReadLine());
+    ThirdDigitStr(x);
     ThirdDigit(x);
 } catch {
     Console.WriteLine("Try again!");
 }
 
-void ThirdDigit(int x) {
+void ThirdDigitStr(int x) {
     string number = x.ToString();
     if(number.Length >= 3) {
         Console.WriteLine(number[2]);
     } else {
         Console.WriteLine("No third digit!");
     }    
+}
+
+void ThirdDigit(int x) {
+    while(x >= 1000) {
+        x = x/10;
+    }
+    if(x > 99) {
+        Console.WriteLine(x % 10);
+    } else {
+        Console.WriteLine("No third digit!");
+    }
 }
