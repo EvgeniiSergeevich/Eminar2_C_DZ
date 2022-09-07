37c35db554d6031c9503d5d91e03f157c396b329
Evgeniy <zheinirk38@gmail.com>
2022-09-07 11:21:04 +0800

    Completed task 4

---
+++ 4th/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter the number: ");
+try {
+    int x = Convert.ToInt32(Console.ReadLine());
+    RemoveDigit(x);
+} catch {
+    Console.WriteLine("I`m asked you to enter NUMBER!!11!");
+}
+
+void RemoveDigit(int x) {
+    string s = Convert.ToString(x);
+    if(s.Length > 1) {
+        s = s.Remove(1, 1);
+        Console.WriteLine(s);
+    } else {
+        Console.WriteLine("There is no second digit!");
+    }    
+}
